afd5bd147031ab0a467ab5baa98f3b7657c60839
Macbook <macbook@Macbooks-MacBook-Air.local>
2024-07-14 15:38:44 +0545

    add airtable api

---
+++ netlify/functions/airtable.js
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import fetch from "node-fetch"
+
+export const handler = async()=> {
+    const airtable_api = 'https://api.airtable.com/v0/appzLdIRMrSF1cK9O/NFC%20Staff?maxRecords=3&view=Grid%20view'
+    const bearer = 'Bearer' + process.env.Bearer_token
+    const response = await fetch(airtable_api, {
+        method: 'GET',
+        headers: {
+            'Authorization': bearer
+        }  
+    })
+    const data = await response.json()
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            data
+        })
+    }
+}
