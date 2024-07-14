f6f01f0364368d5b7bb6b59706fd0b07fae0cd62
Macbook <macbook@Macbooks-MacBook-Air.local>
2024-07-14 16:14:19 +0545

    add api secret key

--- netlify/functions/airtable.js
+++ netlify/functions/airtable.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch"
 
 export const handler = async()=> {
     const airtable_api = 'https://api.airtable.com/v0/appzLdIRMrSF1cK9O/NFC%20Staff?maxRecords=3&view=Grid%20view'
-    const bearer = 'Bearer' + process.env.Bearer_token
+    const bearer = 'Bearer' + process.env.API_SECRET
     const response = await fetch(airtable_api, {
         method: 'GET',
         headers: {
